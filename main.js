c6be50e98a67e567bd3852c0d8005cc2bcbb1798
pb1646a <pb1646a@gmail.com>
2019-03-31 19:59:37 -0400

    added main.js

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 require('backend/server');
 const app = require('backend/app');
+const express = require('express');
+const path = require('path');
+app.use(express.static(__dirname + '/dist/metro-bus-app'));
+
+app.get('/', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/metro-bus-app/index.html'));
+});
