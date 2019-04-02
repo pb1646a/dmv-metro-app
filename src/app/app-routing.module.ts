d5c9176eae3ef0dba36e69bb2ba35f7b4a4c6eac
pb1646a <pb1646a@gmail.com>
2019-04-02 18:50:16 -0400

    added redirect to home for url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
-import { MetroBusComponent } from './metro/metro-bus/metro-bus/metro-bus.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './metro/dashboard/dashboard.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [{
-  path:'', redirectTo:'/metro', pathMatch:'full'},
-{path:'**', redirectTo:'/metro', pathMatch:'full'}
+const routes: Routes = [
+  { path: "", redirectTo: "/metro", pathMatch: "full" },
+  {path:"/metro/metrobus",pathMatch:"full"}
+  { path: "**", redirectTo: "/metro", pathMatch: "full" }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
