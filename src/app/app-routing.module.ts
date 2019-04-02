17e77c38c568e74adaa2d8d09c59a3f1728017c8
pb1646a <pb1646a@gmail.com>
2019-04-02 17:53:02 -0400

    temp fix for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
+import { MetroBusComponent } from './metro/metro-bus/metro-bus/metro-bus.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './metro/dashboard/dashboard.component';
 
 
 const routes: Routes = [{
-  path:'', redirectTo:'/metro', pathMatch:'full'
-}];
+  path:'', redirectTo:'/metro', pathMatch:'full'},
+{path:'metro', component:DashboardComponent }
+{path:'metro/metrobus', component: MetroBusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
